c80934f4cf1e132e9ffab460bf88a70a279926eb
ohy9205 <ohy9205@naver.com>
2022-10-12 16:09:23 +0900

    더미데이터 생성

---
+++ src/util/dummy.js
@@ -0,0 +1,34 @@
+const dummy = [
+  {
+    type: "income",
+    name: "월급",
+    date: 1669042800003,
+    money: 200000000,
+  },
+  {
+    type: "expenses",
+    name: "저녁1",
+    date: 1669042800002,
+    money: 50000,
+  },
+  {
+    type: "expenses",
+    name: "점심",
+    date: 1669042800001,
+    money: 30000,
+  },
+  {
+    type: "expenses",
+    name: "리액트 인강",
+    date: 1668870000000,
+    money: 48000,
+  },
+  {
+    type: "income",
+    name: "월급",
+    date: 1668870000001,
+    money: 50000,
+  },
+];
+
+export default dummy;

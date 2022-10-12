0120eb090d96e92f367bc3826a934bf1f2312aee
ohy9205 <ohy9205@naver.com>
2022-10-12 16:15:22 +0900

    account, list state 생성

--- src/App.js
+++ src/App.js
@@ -4,8 +4,32 @@ import "./App.css";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import List from "./pages/List";
+import { useReducer } from "react";
+
+const accountReducer = (account, action) => {
+  return {
+    total: action.income + action.expenses,
+    income: action.income,
+    expenses: action.expenses,
+  };
+};
+
+const listReducer = (list, action) => {
+  let newList = {};
+  return newList;
+};
 
 function App() {
+  /** 돈정보를 저장하는 state */
+  const [account, accountDispatch] = useReducer(accountReducer, {
+    total: 0,
+    income: 0,
+    expenses: 0,
+  });
+
+  /** 내역리스트 저장하는 state */
+  const [list, listDispatch] = useReducer(listReducer, {});
+
   return (
     <div className="App">
       <Header />

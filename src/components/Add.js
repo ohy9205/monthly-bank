f73cea050a51ab7b636acad9f134bacabbe88b5a
ohy9205 <ohy9205@naver.com>
2022-10-14 15:58:39 +0900

    Add컴포넌트 마크업 수정

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Button from "./Button";
 import Nav from "./Nav";
 
 const Add = () => {
@@ -6,16 +7,40 @@ const Add = () => {
     <div className="add-wrapper">
       <h1>내역 추가</h1>
       <div className="add-list">
-        <label htmlFor="datee">날짜</label>
-        <input type="date" id="datee" />
+        <div className="type-wrapper">
+          <Button className={"income-btn type-on"} text="지출" />
+          <Button className={"expenses-btn"} text="수입" />
+        </div>
+        <label htmlFor="date">날짜</label>
+        <input
+          type="date"
+          id="date"
+          name="date"
+          value={dateFormat(data.date)}
+          onChange={handleChangeState}
+        />
         <label htmlFor="name">설명</label>
-        <input type="text" id="name" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={data.name}
+          onChange={handleChangeState}
+        />
         <label htmlFor="money">금액</label>
-        <input type="number" id="money" min="1" max="999999999" />
+        <input
+          type="number"
+          id="money"
+          name="money"
+          min="1"
+          max="999999999"
+          value={data.money}
+          onChange={handleChangeState}
+        />
       </div>
       <div className="add-btn">
-        <button className="cancle-btn">취소</button>
-        <button className="submit-btn">등록</button>
+        <Button className={"cancle-btn"} text="취소" />
+        <Button className={"submit-btn"} text="등록" type="active" />
       </div>
     </div>
   );

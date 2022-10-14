0b8d3c483ec5edf534e0d7aa7245913367ff678c
ohy9205 <ohy9205@naver.com>
2022-10-14 14:08:54 +0900

    filtering 구현

--- src/components/ControlMenu.js
+++ src/components/ControlMenu.js
@@ -1,12 +1,30 @@
-const ControlMenu = () => {
+import { useEffect, useRef, useState } from "react";
+
+const ControlMenu = ({ onClick }) => {
+  /** 필터 클릭시 동작 함수 */
+  const clickToggle = (e, type) => {
+    let btn = document.querySelectorAll(".filter");
+    for (let it of btn) {
+      it.classList.remove("filter-on");
+    }
+    e.target.classList.add("filter-on");
+
+    onClick(type);
+  };
+
   return (
-    <div className="filter">
-      <input type={"radio"} name="filtering" id="filter-all" />
-      <label htmlFor="filter-all">총</label>
-      <input type={"radio"} name="filtering" id="filter-income" />
-      <label htmlFor="filter-income">수입</label>
-      <input type={"radio"} name="filtering" id="filter-expenses" />
-      <label htmlFor="filter-expenses">지출</label>
+    <div className="filter-wrapper">
+      <button
+        className={`filter filter-on`}
+        onClick={(e) => clickToggle(e, "all")}>
+        총
+      </button>
+      <button className={`filter`} onClick={(e) => clickToggle(e, "income")}>
+        수입
+      </button>
+      <button className={`filter`} onClick={(e) => clickToggle(e, "expenses")}>
+        지출
+      </button>
     </div>
   );
 };
--- src/pages/List.js
+++ src/pages/List.js
@@ -6,24 +6,23 @@ import { DataContext } from "../App";
 const List = () => {
   const { monthData } = useContext(DataContext);
   const [sortedData, setSortedData] = useState();
-  const [filter, setFilter] = useState("filter-all");
+  const [filter, setFilter] = useState("all");
 
-  /** filtering 동작할 함수 */
-  const filterAction = (type) => {
+  /** filtering 동작 */
+  useEffect(() => {
     let filterData = [];
-    if (type) {
-      filterData = monthData.filter((it) => it.type === type).sort(compare);
-    } else {
+    if (monthData.length > 0 && filter === "all") {
       filterData = monthData.sort(compare);
+    } else {
+      filterData = monthData.filter((it) => it.type === filter).sort(compare);
     }
     setSortedData(filterData);
-  };
+  }, [monthData, filter]);
 
   /** 내역 내림차순으로 정렬 */
   const compare = (a, b) => {
     return b.date - a.date;
   };
-  // const sortedData = monthData.sort(compare);
 
   /** 내역 일자 headText 전달 유무를 따지기 위한 변수 */
   let prevDate = 0;
@@ -35,7 +34,7 @@ const List = () => {
           {/* filtering */}
           <h1>월간 내역</h1>
           {/* 총, 지출, 수입 선택시 동작할 함수 생성하고 props로 전달 */}
-          <ControlMenu onClick={filterAction} />
+          <ControlMenu onClick={setFilter} />
         </header>
         <article>
           {sortedData &&

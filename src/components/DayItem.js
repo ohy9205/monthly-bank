6e82676e1aae73aebd7ea72c91343637bd67288c
ohy9205 <ohy9205@naver.com>
2022-10-14 21:25:57 +0900

    내역 리스트 금액 포맷 변경

--- src/components/DayItem.js
+++ src/components/DayItem.js
@@ -2,10 +2,12 @@ import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const DayItem = ({ name, type, money, id }) => {
+  let moneyFormat = parseInt(money).toLocaleString();
+
   return (
     <dl className={type}>
       <dt>{name}</dt>
-      <dd>{type === "income" ? `+${money}` : `-${money}`}</dd>
+      <dd>{type === "income" ? `+${moneyFormat}` : `-${moneyFormat}`}</dd>
       <button>
         <FontAwesomeIcon className="del-btn" icon={faCircleXmark} />
       </button>

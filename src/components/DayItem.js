bb8ebe1e13fc4da676c67598cfb17688a68ccc72
ohy9205 <ohy9205@naver.com>
2022-10-17 16:46:00 +0900

    onEdit, onRemove 이벤트 추가

--- src/components/DayItem.js
+++ src/components/DayItem.js
@@ -3,12 +3,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useContext } from "react";
 import { DispatchContext } from "../App";
 
-const DayItem = ({ name, type, money, id }) => {
+const DayItem = ({ name, type, money, id, onClick }) => {
   let moneyFormat = parseInt(money).toLocaleString();
-  let { onEdit, onRemove } = useContext(DispatchContext);
+  let { onRemove } = useContext(DispatchContext);
 
   return (
-    <dl className={type}>
+    <dl
+      className={type}
+      onClick={(e) => {
+        let targetName = e.target.tagName;
+        (targetName === "DL" || targetName === "DT" || targetName === "DL") &&
+          onClick(id);
+      }}>
       <dt>{name}</dt>
       <dd>{type === "income" ? `+${moneyFormat}` : `-${moneyFormat}`}</dd>
       <button>

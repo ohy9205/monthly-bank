838237416b9eff265a577f4cef194eb5631c980b
yeon <ohy9205@naver.com>
2022-11-30 14:30:31 +0900

    기존 DataContext제거

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import Header from "./components/Header";
 import List from "./components/List";
 import ItemProvider from "./store/ItemProvider";
 
-/** context 생성 */
-export const DataContext = React.createContext();
-export const DispatchContext = React.createContext();
-
 function App() {
   return (
     <ItemProvider>

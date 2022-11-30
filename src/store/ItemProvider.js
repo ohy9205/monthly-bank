84c6990cfd75eca2f01a93bff1b2ddd6e771dd1b
yeon <ohy9205@naver.com>
2022-12-01 02:04:10 +0900

    update메소드에 useCallback추가

--- src/store/ItemProvider.js
+++ src/store/ItemProvider.js
@@ -1,4 +1,4 @@
-import { useEffect, useReducer, useRef, useState } from "react";
+import { useCallback, useEffect, useReducer, useRef, useState } from "react";
 import ItemContext from "./item-context";
 
 // 내역 리스트 reducer
@@ -48,19 +48,60 @@ const ItemProvider = ({ children }) => {
     }
   }, []);
 
+  /** 해당 월에 해당되는 내역 추출 */
+  const updateMonthData = useCallback(() => {
+    const thisMonthfirstDay = new Date(
+      curDate.getFullYear(),
+      curDate.getMonth(),
+      1
+    ).getTime();
+
+    const nextMonthfirstDay = new Date(
+      curDate.getFullYear(),
+      curDate.getMonth() + 1,
+      1
+    ).getTime();
+
+    setMonthData(
+      list.filter((it) => {
+        return (
+          parseInt(thisMonthfirstDay) <= parseInt(it.date) &&
+          parseInt(nextMonthfirstDay) > parseInt(it.date)
+        );
+      })
+    );
+  }, [curDate, list]);
+
+  /** 월 돈 정보 업데이트 */
+  const updateAccount = useCallback(() => {
+    let total = 0;
+    let income = 0;
+    let expenses = 0;
+
+    for (let item of monthData) {
+      income += item.type === "INCOMES" && parseInt(item.money);
+      expenses += item.type === "EXPENSES" && parseInt(item.money);
+    }
+    total = income - expenses;
+    setAccount({
+      total: total.toString(),
+      income: income.toString(),
+      expenses: expenses.toString(),
+    });
+  }, [monthData]);
+
   /** 내역 수정이나 월 변경 시 account, monthData state 업데이트 */
   useEffect(() => {
     // list가 빈값이 아니면 account 상태 업데이트
     if (list.length < 0) return;
     updateMonthData();
-    // updateAccount();
-  }, [list, curDate]);
+  }, [list, curDate, updateMonthData]);
 
   /** 월 데이터가 바뀌면 돈 정보도 업데이트*/
   useEffect(() => {
     updateAccount();
     itemId.current = monthData.length + 1;
-  }, [monthData]);
+  }, [monthData, updateAccount]);
 
   /** 내역 초기화 */
   const initItem = (items) => {
@@ -99,51 +140,6 @@ const ItemProvider = ({ children }) => {
   /** 월 변경 */
   const changeMonth = (month) => {
     setCurDate(new Date(curDate.getFullYear(), month, curDate.getDate()));
-  };
-
-  /** 해당 월에 해당되는 내역 추출 */
-  const updateMonthData = () => {
-    const firstDay = new Date(
-      curDate.getFullYear(),
-      curDate.getMonth(),
-      1
-    ).getTime();
-
-    const lastDay = new Date(
-      curDate.getFullYear(),
-      curDate.getMonth() + 1,
-      0,
-      23,
-      59,
-      59
-    ).getTime();
-
-    setMonthData(
-      list.filter((it) => {
-        return (
-          parseInt(firstDay) <= parseInt(it.date) &&
-          parseInt(lastDay) >= parseInt(it.date)
-        );
-      })
-    );
-  };
-
-  /** 월 돈 정보 업데이트 */
-  const updateAccount = () => {
-    let total = 0;
-    let income = 0;
-    let expenses = 0;
-
-    for (let item of monthData) {
-      income += item.type === "INCOMES" && parseInt(item.money);
-      expenses += item.type === "EXPENSES" && parseInt(item.money);
-    }
-    total = income - expenses;
-    setAccount({
-      total: total.toString(),
-      income: income.toString(),
-      expenses: expenses.toString(),
-    });
   };
 
   const itemValue = {
